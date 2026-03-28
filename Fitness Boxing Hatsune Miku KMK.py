43287be76456111e63dbdad6c052c43e0e3c0624
LeiAfkpuz <1130707+LeiAfkpuz@users.noreply.github.com>
2026-03-27 21:04:41 -0400

    Uploading file from PC
    Initial attempt - Visual Studio showing errors on every "return" line, but we'll see what happens!

---
+++ Fitness Boxing Hatsune Miku KMK.py
@@ -0,0 +1,85 @@
+from __future__ import annotations
+
+import functools
+from typing import List, Dict, Set
+
+from dataclasses import dataclass
+
+from Options import Toggle, OptionSet
+
+from ..game import Game
+from ..game_objective_template import GameObjectiveTemplate
+
+from ..enums import KeymastersKeepGamePlatforms
+
+@dataclass
+class FitnessBoxingHatsuneMikuArchipelagoOptions:
+    #miku_boxing_difficulties: MikuBoxingDifficulties
+    pass
+
+class FitnessBoxingHatsuneMikuGame(Game):
+    name = "Fitness Boxing feat. HATSUNE MIKU"
+    platforms = KeymastersKeepGamePlatforms.SW
+
+    is_adult_only_or_unreated = False
+
+    options_cls = FitnessBoxingHatsuneMikuArchipelagoOptions
+
+    def game_objective_templates(self) -> List[GameObjectiveTemplate]:
+    return [
+        GameObjectiveTemplate(
+            label="Play SONG on DIFFICULTY difficulty"
+            data={
+                "SONG": (self.miku_exercise_list, 1),
+                "DIFFICULTY": (self.difficulties_list, 1),
+            }
+            is_time_consuming=False,
+            is_difficult=False,
+            weight=3
+        ),
+    ]
+
+    @staticmethod
+    def miku_exercise_list() -> List[str]:
+    return [
+        "I Will Make You Miku Miku(Miku Miku ni Shiteageru)",
+        "Melt",
+        "THE END OF HATSUNE MIKU",
+        "Fire Flower",
+        "Po Pi Po",
+        "Double Lariat",
+        "Romeo & Cinderella",
+        "Just Be Friends",
+        "melancholic",
+        "World's End Dancehall",
+        "8HIT",
+        "Tell Your World",
+        "Ai Dee",
+        "Lost One's Weeping",
+        "Burikinodansu",
+        "Alien Alien",
+        "Teo",
+        "Hated By Life",
+        "BRING IT ON",
+        "Shoujo Rei",
+        "Bitter Choco Decoration",
+        "Telecaster b boy (long ver.)",
+        "The Vampire",
+        "God-ish",
+        "Rise up",
+        "Medic: A Doll",
+        "OVER LIT",
+        "Lovesong ni Shukuhaiwo!",
+        "Champion Road to True Love",
+        "Let's Mikusercise!!",
+        "watashi no audiro",
+    ]
+
+    @staticmethod
+    def difficulties_list() -> List[str]:
+    return [
+        "Lightweight",
+        "Middlweight",
+        "Heavyweight",
+    ]
+
